5c91dbeee0d330a16ca9ca812846bcea919c3b1d
Ansari Abdurrahman gufran Ahmed <110079179+ibnegufran@users.noreply.github.com>
2025-12-13 18:38:53 +0530

    img

--- client/src/pages/home/Hero.jsx
+++ client/src/pages/home/Hero.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+// import img from "./src/pu"
 const Hero = () => {
   return (
     <section className="w-full md:h-[90vh] bg-white  py-20 md:pb-0 px-6">
@@ -21,7 +23,7 @@ const Hero = () => {
         {/* Right Image */}
         <div className="flex-1">
           <img
-            src="./images/hero-image.png"
+            src="../images/hero-image.png"
             alt="FundEase Hero"
             className=" w-full rounded-full hero-img"
           />
